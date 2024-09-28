0e80bd3e11861a6c27d6cde12cff162cf2b7d852
HamzaLamrabetehb <hamza.lamrabet@student.ehb.be>
2024-09-28 19:22:46 +0200

    Making of own user's space spaceInvader

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,57 @@
 "use strict";
 
 alert("Draw your space invader here");
+
+console.log("test");
+
+drawline();
+
+function drawline() {
+	let canvas = document.querySelector("canvas");
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	let context = canvas.getContext("2d");
+
+	context.lineWidth = 10;
+	context.strokeStyle = "black";
+	context.beginPath();
+	context.rect(100, 100, 300, 300);
+	context.stroke();
+	context.fill();
+
+	context.fillStyle = "cyan";
+	context.beginPath();
+	context.rect(125, 150, 50, 100);
+	context.stroke();
+	context.fill();
+
+	context.fillStyle = "cyan";
+	context.beginPath();
+	context.rect(175, 200, 50, 100);
+	context.fill();
+
+	context.fillStyle = "cyan";
+	context.beginPath();
+	context.rect(125, 300, 100, 50);
+	context.fill();
+
+	context.fillStyle = "cyan";
+	context.beginPath();
+	context.rect(225, 250, 50, 50);
+	context.fill();
+
+	context.fillStyle = "cyan";
+	context.beginPath();
+	context.rect(275, 200, 50, 100);
+	context.fill();
+
+	context.fillStyle = "cyan";
+	context.beginPath();
+	context.rect(325, 150, 50, 100);
+	context.fill();
+
+	context.fillStyle = "cyan";
+	context.beginPath();
+	context.rect(275, 300, 100, 50);
+	context.fill();
+}
