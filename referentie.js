f79123a3dee38c374073e906f136ea88267bc27c
HamzaLamrabetehb <hamza.lamrabet@student.ehb.be>
2024-12-18 23:28:02 +0100

    TAAK KLAAR Hamza Lamrabet

---
+++ referentie.js
@@ -0,0 +1,2 @@
+// Een deel van deze code is gemaakt dankzij het gebruikt van CHAT GPT, zoals aangekondigd met het hoorcolleges zal een link geplaats waar alle interactie die ik had met chat gpt voor mijn "interactive design" code komen voor:
+//https://chatgpt.com/share/67634b4b-d670-800d-9d98-2b1453b1df49
